301a21a845865b887265225fc9c4ba8ac30d6fd7
philippe gabriel <philippegabriel@users.noreply.github.com>
2023-11-28 14:32:15 +0000

    Add pytest workflow

--- leetcode238.py
+++ leetcode238.py
@@ -34,7 +34,10 @@ class Solution:
             i += 1
         return result
 
-if __name__ == "__main__":
+def test() -> None:
     s=Solution()
     assert s.productExceptSelf([1,2,3,4]) == [24,12,8,6]
     assert s.productExceptSelf([-1,1,0,-3,3]) == [0,0,9,0,0]
+
+if __name__ == "__main__":
+    test()
--- test_leetcode.py
+++
@@ -1,5 +0,0 @@
-''' Test code for leetcode75'''
-import leetcode41
-
-if __name__ == "__main__":
-    leetcode41.test()
