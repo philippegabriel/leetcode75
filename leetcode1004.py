1b4259d90a5a182fc22bc1526cf1004b0f273ca2
philippe gabriel <philippegabriel@hotmail.com>
2024-02-02 19:04:37 +0000

    grinding...

--- leetcode1004.py
+++ leetcode1004.py
@@ -6,37 +6,28 @@ from collections import deque
 class Solution:
     # pylint: disable-next=invalid-name
     def longestOnes(self, nums: List[int], k: int) -> int:
+        if not nums:
+            return 0
         l=0
         zp:deque[int]=deque()
-        #initialise l and r
-        for i,num in enumerate(nums):
-            if num:
-                continue
-            #process zero
-            if len(zp) == k:
-                break
-            zp.append(i)
-        #Found init sequence
-        if i == len(nums)-1:
-            return len(nums)
-        #l and r are initialised
-        #find other sequences
         result = 0
-        for r,num in enumerate(nums[i:],i):
-            if num:
-                continue
-            if len(zp) == k:
-                result = max(result,r-l)
-                #reset l
-                l=zp[0]+1
-                zp.popleft()
-            zp.append(r)
+        for r,num in enumerate(nums):
+            if not num:
+                zp.append(r)
+                if len(zp) == k+1:
+                    #reset l
+                    l=zp[0]+1
+                    zp.popleft()
+            result = max(result,r-l+1)
         return result
 
 def test() -> None:
     s=Solution()
+    print(s.longestOnes(nums = [], k = 2))
     print(s.longestOnes(nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2))
+    print(s.longestOnes(nums = [1,1,1,0,0,0,1,1,1,1,0], k = 0))
     print(s.longestOnes(nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3))
+    print(s.longestOnes(nums = [1]*100, k = 3))
 
 if __name__ == "__main__":
     test()
